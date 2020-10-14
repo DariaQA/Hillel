2ef43d5b9b0c30fda67ec2c0ecea979586474716
DariaQA <dawulikis@gmail.com>
2020-10-14 15:34:20 +0300

    Add New File

---
+++ MyFirstProgram.java
@@ -0,0 +1,11 @@
+public class MyFirstProgram {
+    public static void main(String[]  args) {
+
+        System.out.println("Hello, I'm Daria!") ;
+        System.out.println("I'm 31 years old") ;
+        System.out.println("My work is QA engineer") ;
+        System.out.println("I think that autotests will help me to save a lot of time for regression testing.") ;
+        System.out.println("I learned a little Java at SoloLearn") ;
+        System.out.println("In my free time I read, play and clean))))))") ;
+    }
+}
---
+++ MyInitials.java
@@ -0,0 +1,47 @@
+public class MyInitials {
+    public static void main(String[] args) {
+        System.out.println("My initials are:");
+        System.out.print("PPPPPPPPPPPP");
+        System.out.print("      DDDDDDD ");
+        System.out.println("            OOO");
+        System.out.print("PPPPPPPPPPPPPP");
+        System.out.print("    DDDDDDDDD ");
+        System.out.println("        OOOOOOO");
+        System.out.print("PPPP       PPPP");
+        System.out.print("   DDDD    DD ");
+        System.out.println("      OOO   OOO");
+        System.out.print("PPPP       PPPP");
+        System.out.print("   DDDD    DDD");
+        System.out.println("     OOOO   OOOO");
+        System.out.print("PPPP       PPPP");
+        System.out.print("   DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP       PPPP");
+        System.out.print("   DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP       PPPP");
+        System.out.print("   DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPPPPPPPPPPPP");
+        System.out.print("    DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPPPPPPPPPPP ");
+        System.out.print("    DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP          ");
+        System.out.print("    DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP          ");
+        System.out.print("    DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP          ");
+        System.out.print("    DDDD    DDDD");
+        System.out.println("    OOOO   OOOO");
+        System.out.print("PPPP          ");
+        System.out.print("    DDDDDDDDDDD   ");
+        System.out.println("   OOOOOOOOO");
+        System.out.print("PPPP          ");
+        System.out.print("    DDDDDDDDD     ");
+        System.out.println("      OOO");
+    }
+}
